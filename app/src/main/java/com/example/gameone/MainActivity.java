e37367c3e418e3a2a7e5341e60cbad147ff40970
tirigun <terigun@csii.com.cn>
2021-01-15 14:59:44 +0800

    new

--- app/src/main/java/com/example/gameone/MainActivity.java
+++ app/src/main/java/com/example/gameone/MainActivity.java
@@ -1,17 +1,49 @@
 package com.example.gameone;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private Button bt_start;
+    private Button bt_continue;
+    private Button bt_stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
         //
     }
 
+    private void initView() {
+        bt_start = (Button) findViewById(R.id.bt_start);
+        bt_continue = (Button) findViewById(R.id.bt_continue);
+        bt_stop = (Button) findViewById(R.id.bt_stop);
+
+        bt_start.setOnClickListener(this);
+        bt_continue.setOnClickListener(this);
+        bt_stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_start:
+                startActivity(new Intent(MainActivity.this,DrawActivity.class));
+                break;
+            case R.id.bt_continue:
+
+                break;
+            case R.id.bt_stop:
+                finish();
+                break;
+        }
+    }
 }
